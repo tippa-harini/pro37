dde38ca8bbb195a5c27fdbd9b41c1415cb46526b
tippa-harini <73785126+tippa-harini@users.noreply.github.com>
2021-06-12 20:18:17 +0530

    Add files via upload

--- js/Quiz.js
+++ js/Quiz.js
@@ -28,36 +28,39 @@ class Quiz {
     }
   }
 
-  play(){
-    //write code here to hide question elements
-    question.hide()
+   play(){ 
+     //write code here to hide question elements
 
-    //write code to change the background color here
-
-    //write code to show a heading for showing the result of Quiz
-    textSize(30);
-    text("Result Of The Quiz",310,80);
-
-    //call getContestantInfo( ) here
-
-    Contestant.getPlayerInfo();
-
-    //write condition to check if contestantInfor is not undefined
-    if(allContestants !== undefined){
-      fill("blue");
-      textSize(20);
-      text("*Note: Contestant who answered  correct are highlighted in green colour! ")
-    }
-     for(var plr in allContestants){
-       var correctAns= "2"
-       if(correctAns=== allContestants[plr].answer){
-         fill ("green")
-       }else 
-       fill("red");
-         
-       
-     }
+    question.hide() 
     
-  }
+    //write code to change the background color here
+    
+     //write code to show a heading for showing the result of Quiz 
+
+     textSize(30); 
+     text("Result Of The Quiz",310,80);
+     
+     //call getContestantInfo( ) here 
+
+     Contestant.getPlayerInfo(); 
+      
+     //write condition to check if contestantInfor is not undefined 
+      if(allContestants !== undefined){
+        var display_Answers = 230; 
+        fill("blue"); textSize(20);
+        text("*Note: Contestant who answered correct are highlighted in green colour! ")
+      }
+
+      for(var plr in allContestants){ 
+        var correctAns= "2" 
+        if(correctAns=== allContestants[plr].answer){
+        fill ("green")
+        }else {
+        fill("red");
+        } display_Answers+=30;
+        textSize(20);
+        text(allContestants[plr].name + ": " + allContestants[plr].answer, 250,display_Answers)
+        }
+     }
 
 }
